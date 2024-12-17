0ea2367dcac4f12ab5368c204474880e851b5cfb
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2024-12-18 00:11:21 +0100

    client & server final

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import { Josefin_Sans } from "next/font/google";
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
 import Header from "./_components/Header";
+import { ReservationProvider } from "./_components/ReservationContext";
 
 export const metadata = {
   title: {
@@ -27,7 +28,9 @@ export default function RootLayout({ children }) {
         <Header />
 
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
